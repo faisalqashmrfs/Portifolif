033c23a7d8665f3305820b1a9be879f8fb49e6b9
faisalqashmrfs <125458443+faisalqashmrfs@users.noreply.github.com>
2024-10-29 20:06:12 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,28 @@ import './App.css'
 import PortfolioContent from './Components/PortfolioContent/PortfolioContent';
 import LoadingScreen from './Components/Looder/Looder';
 import StarryBackground from './Animation/Start/start';
+import { AiOutlineCodepen } from "react-icons/ai";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [Mode, setMode] = useState(false);
 
   const handleLoaded = () => {
     setLoading(false); // عندما يتم الانتهاء من التحميل، قم بإخفاء شاشة التحميل
   };
 
   return (
-    <main>
+    <main className={Mode? 'desmain' : 'main'}>
       <StarryBackground/>
       {!loading ? (
         <LoadingScreen onLoaded={handleLoaded} />
       ) : (
-        <PortfolioContent /> // هذا هو المحتوى الأساسي للبورتفوليو
+        // هذا هو المحتوى الأساسي للبورتفوليو
+        <div> 
+          <button className='ModeChanger' onClick={() => setMode(!Mode)}><AiOutlineCodepen />
+          </button>
+          <PortfolioContent />
+        </div> 
       )}
     </main>
   )
--- src/Components/AboutMe/AboutMe.jsx
+++ src/Components/AboutMe/AboutMe.jsx
@@ -6,47 +6,22 @@ import './AboutMe.css'
 
 export default function AboutMe() {
 
-  const [borderRadius, setBorderRadius] = useState("");
-  const [transform, setTransform] = useState("");
-
-  // دالة لتوليد القيم العشوائية لـ border-radius
-  const randomBorderRadius = () => {
-    return `${Math.random() * 80 + 60}% ${Math.random() * 80 + 40}% ${Math.random() * 60 + 40}% ${Math.random() * 80 + 40}% / ${Math.random() * 80 + 40}% ${Math.random() * 80 + 40}% ${Math.random() * 80 + 40}% ${Math.random() * 80 + 40}%`;
-  };
-
-  // دالة لتغيير حركة وحجم الـ blob
-  const randomMovement = () => {
-    const randomX = Math.random() * 0 ;
-    const randomY = Math.random() * 0 ;
-    const randomScale = Math.random() * 0 + 2;
-
-    setBorderRadius(randomBorderRadius());
-    setTransform(`translate(${randomX}px, ${randomY}px) scale(${randomScale})`);
-  };
-
-  // استخدام useEffect لتطبيق الحركة العشوائية بشكل مستمر
-  useEffect(() => {
-    const interval = setInterval(randomMovement, 800); // تغيير كل 1.5 ثانية
-    return () => clearInterval(interval); // تنظيف التايمر عند تفكيك المكون
-  }, []);
-
-
 
 
   return (
     <section className='About-me'>
       <h2> I'M A  
       <Typewriter
-        words={[' Full Stack Developer', ' UI/UX Designer', ' Web Trainer' , '' , '']}
+        words={[' Full Stack Developer', ' UI/UX Designer', ' Web Trainer']}
         loop={0}
          deleteSpeed={35}
         cursor
         cursorStyle='|'
-        typeSpeed={60}
+        typeSpeed={50}
        delaySpeed={1000}
       />
       </h2>
-      <div className="blob" style={{ borderRadius, transform }}>
+      <div className="blob">
           <img src={me} alt="Profile" />
         </div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo illum soluta necessitatibus reiciendis nihil culpa eos labore, deleniti fugiat iure debitis sequi ducimus qui, error, voluptatum consequuntur itaque tempora ratione!</p>
